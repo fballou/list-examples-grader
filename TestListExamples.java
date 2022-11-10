f7cf0e47e5d3c151d6bbdd236887d619c2fdb0ee
fballou <fballou@ucsd.edu>
2022-11-10 13:49:50 -0800

    week7Lab

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,26 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
 
 public class TestListExamples {
   // Write your grading tests here!
+  
+  @Test
+    public void largeList() {
+        ArrayList<String> input = new ArrayList<>(Arrays.asList("swag", "weather and stuff", "and", "andy", "sweet"));
+        ArrayList<String> expected = new ArrayList<>(Arrays.asList("weather and stuff", "and", "andy"));
+
+        List<String> test = ListExamples.filter(input, new HasAnAnd());
+
+        assertArrayEquals(expected.toArray(), test.toArray());
+    }
+}
+
+class HasAnAnd implements StringChecker {
+  public boolean checkString(String s) {
+      if (s.contains("and")) {
+          return true;
+      }
+      return false;
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,26 @@
 # Create your grading script here
 
+CPATH=".:/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 set -e
 
 rm -rf student-submission
+mkdir student-submission
+
 git clone $1 student-submission
 
+if [[-f student-submission/ListExamples.java]] 
+then
+    echo "Files were found chief"
+else
+    echo "Files were not found chief"
+    exit 39
+fi
+
+
+cp TestListExamples.java student-submission
+cp -r lib student-submission
+cd student-submission
+
+javac -cp $CPATH *.java
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
